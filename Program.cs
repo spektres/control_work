c0eee2706276cdc5c9237ec41d7c307ab618cbe5
spektres <spektres@mail.ru>
2022-10-03 18:00:48 +0300

    the script is executed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[4] {"hello", "2", "world", ":-)"};
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            Console.Write($"\"{array[i]}\",");
+        }
+    }
+    Console.Write("]");
+}
+
+PrintArray(array);
